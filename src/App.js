08f207df9377597b84bd2d611c058e70522ccdc6
MickJerin12 <s.mickjerin@outlook.com>
2021-04-04 11:11:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { commerce } from "./lib/commerce";
+import Products from "./components/Products/Products";
+import NavBar from "./components/NavBar/NavBar";
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Products />
+      </div>
+    </Router>
   );
-}
-
+};
 export default App;
